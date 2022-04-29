49d70e22ba251d50c99493cb9b6f954b33e650cf
alfonsocq <alfcastroq@gmail.com>
2022-04-28 19:14:23 -0500

    core

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import PersonCard from "./components/PersonCard"
 function App() {
   return (
     <div className="App">
-      <PersonCard nombre="Juan" apellido="Depp" edad={45} colorcabello="rojo"/>
-      <PersonCard nombre="Pedro" apellido="Solez" edad={15} colorcabello="negro"/>
-      <PersonCard nombre="Hector" apellido="Diaz" edad={5} colorcabello="blanco"/>
-      <PersonCard nombre="Amber" apellido="Sotoz" edad={25} colorcabello="verde"/>
+      <PersonCard nombre="Juan" apellido="Depp" edad={45} colordecabello="rojo"/>
+      <PersonCard nombre="Pedro" apellido="Solez" edad={15} colordecabello="negro"/>
+      <PersonCard nombre="Hector" apellido="Diaz" edad={5} colordecabello="blanco"/>
+      <PersonCard nombre="Amber" apellido="Sotoz" edad={25} colordecabello="verde"/>
     </div>
   );
 }
